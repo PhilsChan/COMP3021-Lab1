582bc8b02b3556be477f1804608cbe3abf90d809
PhilsChan <philsonchan@gmail.com>
2018-09-12 17:02:23 +0800

    My first Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // TODO: testing java 10 feature
+        var x = 5;
+        System.out.println(x);
+    }
+}
